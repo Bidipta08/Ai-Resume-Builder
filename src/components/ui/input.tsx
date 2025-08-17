b386997f22276bc0d68f0afccb3995445f9c2223
Bidipta08 <bids8811@gmail.com>
2025-08-17 13:25:54 +0530

    Fix: input and textarea props typing

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,12 +1,7 @@
-import * as React from "react";
+import React from "react";
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 export function Input(props: InputProps) {
-  return (
-    <input
-      {...props}
-      className={`border border-gray-300 rounded-lg px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500 ${props.className || ""}`}
-    />
-  );
+  return <input {...props} className="border p-2 rounded w-full" />;
 }
--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,12 +1,8 @@
-import * as React from "react";
+import React from "react";
 
-interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
 
 export function Textarea(props: TextareaProps) {
-  return (
-    <textarea
-      {...props}
-      className={`border border-gray-300 rounded-lg px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500 ${props.className || ""}`}
-    />
-  );
+  return <textarea {...props} className="border p-2 rounded w-full" />;
 }
+
